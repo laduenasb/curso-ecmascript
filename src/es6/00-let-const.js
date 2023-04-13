9a54bdf914a2a74dc81b12c7d7a7b4ea81742b76
Luis Dueñas <luisdue3@gmail.com>
2023-04-13 18:54:26 -0500

    ES6: let y const, y arrow functions

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,24 @@
+var lastName = 'David';
+lastName = 'Luis';
+console.log(lastName);
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit)
+
+const animal = {tipo: 'Dog'};
+animal.tipo='gato';
+console.log(animal);
+
+const fruits = ()=>{
+	if(true){
+		var fruit1='Apple';
+		let fruit2='Kiwi';
+		const fruit3='Banana';
+	}
+	console.log(fruit1);
+	console.log(fruit2);
+	console.log(fruits3);
+}
+
+fruits();
