5d4605e9850232478105c122cf0dda55d88ac3bc
Luis Dueñas <luisdue3@gmail.com>
2023-04-13 19:45:19 -0500

    ES6: strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,21 @@
+let hello = 'Hello'
+let world = 'World'
+let epicPhrase = hello + ' ' + world + '!'
+// console.log(epicPhrase)
+
+// Template literals
+
+let epicPhrase2 = `${hello} ${world}!`
+
+console.log(epicPhrase2)
+
+// Multi-line strings
+
+let loren = 'esto es un string \n ' + 'esto es otra linea'
+
+let loren2 = `Esta es una frase epica
+	la continuación de esa frase epica.
+`
+
+console.log(loren)
+console.log(loren2)
