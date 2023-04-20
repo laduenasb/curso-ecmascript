6652e17245bdb7db23515909f0a167817f0329e4
Luis Dueñas <luisdue3@gmail.com>
2023-04-20 09:53:09 -0500

    ES6: spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -10,4 +10,28 @@ let user = {username: 'Oscar', age: 34}
 
 let {age, username} = user
 
-console.log(username, user.age);
+console.log(username, user.age)
+
+// Spread operator
+
+let person = {name: 'Oscar', age: 28}
+let country = 'MX'
+
+let data = {id: 1, ...person, country}
+console.log(data)
+
+// rest
+
+function sum(num, ...values) {
+	console.log(values)
+	console.log(num + values[0])
+	return num + values[0]
+}
+
+sum(5,1,2,3)
+
+let kissEmoji = [..."👩‍❤️‍💋‍👩"]
+console.log(kissEmoji)
+
+let familyEmoji = [..."👨‍👩‍👦‍👦"]
+console.log(familyEmoji)
